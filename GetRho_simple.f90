23c57f42609d305da00b4bee81f7794ad0fcab9a
dirk.nowi@gmail.com <dirk.nowi@gmail.com>
2018-05-13 11:09:16 +0800

    all four 1D shocktue test as in Toro(1997)

--- GetRho_simple.f90
+++ GetRho_simple.f90
@@ -44,6 +44,48 @@ elseif(test_model_no == 2) then
    call boundary1d(p,even)
    call boundary1d(epsilon,even)
 
+elseif(test_model_no == 3) then
+   
+   do j=1,length_step
+      if(j <= length_step*4/10) then
+         rho(j) = 5.99924_dp
+         vel(j) = 19.5975_dp
+         p(j) = 460.894_dp
+         epsilon(j) = p(j) / rho(j) / (gamma-1.0_dp)
+      else
+         rho(j) = 5.99242_dp
+         vel(j) = -6.19633_dp
+         p(j) = 46.0950_dp
+         epsilon(j) = p(j) / rho(j) / (gamma-1.0_dp)
+      endif
+   enddo
+   call boundary1d(rho,even)
+   call boundary1d(vel,even)
+   call boundary1d(p,even)
+   call boundary1d(epsilon,even)
+
+elseif(test_model_no == 4) then
+   
+   do j=1,length_step
+      if(j <= length_step*8/10) then
+         rho(j) = 1.0_dp
+         vel(j) = -19.59745_dp
+         p(j) = 1000.0_dp
+         epsilon(j) = p(j) / rho(j) / (gamma-1.0_dp)
+      else
+         rho(j) = 1.0_dp
+         vel(j) = -19.59745_dp
+         p(j) = 0.01_dp
+         epsilon(j) = p(j) / rho(j) / (gamma-1.0_dp)
+      endif
+   enddo
+   call boundary1d(rho,even)
+   call boundary1d(vel,even)
+   call boundary1d(p,even)
+   call boundary1d(epsilon,even)
+
+else
+   stop "no such test model"   
 endif
 
 endsubroutine
