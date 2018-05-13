cf0e234a5ad4b04c67831120633d6b4a2fe2e249
dirk.nowi@gmail.com <dirk.nowi@gmail.com>
2018-05-13 15:51:19 +0800

    Initial perturbation for the oscillation

--- GetVel.f90
+++ GetVel.f90
@@ -5,7 +5,9 @@ IMPLICIT NONE
 INTEGER :: j
 
 DO j = 1, length_step
-	vel (j) = ini_vel
+   IF(rho(j)>=1.1*rho_a) THEN
+      vel (j) = - ini_vel * dx * (DBLE(j)-0.5_dp) 
+   ENDIF
 END DO
 
 CALL BOUNDARY1D (vel, odd)
