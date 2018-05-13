ac68e397b26b71ca2ad08cca32e51116f7921f13
dirk.nowi@gmail.com <dirk.nowi@gmail.com>
2018-05-13 15:12:15 +0800

    differentiate boundary condition for a star and test model

--- Boundary1D.f90
+++ Boundary1D.f90
@@ -6,18 +6,29 @@ REAL (DP), INTENT (INOUT), DIMENSION (-4 : length_step + 5) :: array
 INTEGER, INTENT (IN) :: sign
 INTEGER :: j
 
-IF (sign == even) THEN
-	DO j = 1, 5
-		array (1 - j) = array (1)
-	END DO
-ELSE IF (sign == odd) THEN
-	DO j = 1, 5
-		array (1 - j) = - array (j)
-	END DO
-END IF
+IF(star_model) THEN
 
-DO j = 1, 5
-	array (length_step + j) = array (length_step)
-END DO
+   IF(sign == even) THEN
+      DO j = 1,5
+         array(1-j) = array(j)
+      ENDDO
+   ELSEIF(sign == odd) THEN
+      DO j = 1,5
+         array(1-j) = - array(j)
+      ENDDO
+   ENDIF
+
+   DO j=1, 5
+      array(length_step + j) = array(length_step)
+   ENDDO
+
+ELSEIF(test_model) THEN
+
+   DO j = 1, 5
+      array (1 - j) = array (1)
+      array (length_step + j) = array (length_step)
+   END DO
+
+ENDIF
 
 END SUBROUTINE
--- Boundary2D.f90
+++ Boundary2D.f90
@@ -5,20 +5,29 @@ IMPLICIT NONE
 REAL (DP), INTENT (INOUT), DIMENSION (1 : no_of_eq, -4 : length_step + 5) :: u
 INTEGER :: i, j
 
-DO i = 1, no_of_eq
-	IF (parity2d (i) == even) THEN
-		DO j = 1, 5
-			u (i, 1 - j) = u (i, 1)
-		END DO
-	ELSE IF (parity2d (i) == odd) THEN
-		DO j = 1, 5
-			u (i, 1 - j) = - u (i, j)
-		END DO
-	END IF
+IF(star_model) THEN
+  
+   do j = 1,5
+      u(1,1-j) = u(1,j)
+      u(2,1-j) = - u(2,j)
+      u(3,1-j) = u(3,j)
+   enddo
 
-	DO j = 1, 5
-		u (i, length_step + j) = u (i, length_step)
-	END DO
-END DO
+   do j = 1,5
+      do i = 1,no_of_eq
+         u(i,length_step+j) = u(i,length_step)
+      enddo
+   enddo
+
+ELSEIF(test_model) THEN
+
+   do j=1,5
+      do i=1,no_of_eq
+         u(i,1-j) = u(i,1) 
+         u(i,length_step+j) = u(i,length_step)
+      enddo
+   enddo
+
+ENDIF
 
 END SUBROUTINE
